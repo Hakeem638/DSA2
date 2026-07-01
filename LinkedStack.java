4209c6c39ff2964128dd8c2570c2788b7384d775
Hakeem638 <harunahakeem638@gmail.com>
2026-07-01 03:29:30 +0000

    done with the implementation of the ADT and the various operations

--- LinkedStack.java
+++ LinkedStack.java
@@ -6,7 +6,29 @@ public class LinkedStack {
     public Node (int value, Node next) {
       this.value = value;
       this.next = next;
-      
     }
+
+  }
+
+  private Node top;
+
+  public void push(int value) {
+    top = new Node(value, top);
+  }
+
+  public int pop() {
+    if (top == null) {
+      throw new IllegalStateException("The stack is empty");
+    }
+    int item = top.value;
+    top = top.next;
+    return item;
+  }
+
+  public int peek() {
+    if (top == null) {
+      throw new IllegalStateException("The stack is empty");
+    }
+    return top.value;
   }
 }
