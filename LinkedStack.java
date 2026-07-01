1700456398ded7dea15ce5f5e261eb5e08032d5f
Hakeem638 <harunahakeem638@gmail.com>
2026-07-01 02:58:23 +0000

    Implementing a stack using a linked node instead of an array

---
+++ LinkedStack.java
@@ -0,0 +1,12 @@
+public class LinkedStack {
+  private static class Node {
+    int value;
+    Node next;
+
+    public Node (int value, Node next) {
+      this.value = value;
+      this.next = next;
+      
+    }
+  }
+}
